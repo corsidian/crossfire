f534b6671627370607ed7b17de60834d45e7292b
Corsidian <corsidian04@gmail.com>
2012-06-05 20:59:31 -0400

    Update removing Connection Manager. This should be done in a plugin instead.

---
+++ src/java/net/emiva/crossfire/UnknownStanzaException.java
@@ -0,0 +1,10 @@
+package net.emiva.crossfire;
+
+public class UnknownStanzaException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4905625738319903571L;
+
+}
